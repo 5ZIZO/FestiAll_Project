feb1c3215b2e423f15714e7d8aa2b3e469ca3656
Captain-Kim <brighthero@kakao.com>
2024-06-18 02:56:59 +0900

    add: 로컬 스토리지에 인증 상태 저장하도록 전역 상태 설정

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,18 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const useAuthStore = create((set) => ({
-  accessToken: null,
-  setAccessToken: (token) => set({ accessToken: token }),
-  clearAccessToken: () => set({ accessToken: null }),
-}));
+const useAuthStore = create(
+  persist(
+    (set) => ({
+      accessToken: null,
+      setAccessToken: (token) => set({ accessToken: token }),
+      clearAccessToken: () => set({ accessToken: null }),
+    }),
+    {
+      name: 'accessToken',
+      // getStorage: () => localStorage, 명시적 사용하려 했으나 비권장 사항 확인(레거시?)
+    }
+  )
+);
 
 export default useAuthStore;
