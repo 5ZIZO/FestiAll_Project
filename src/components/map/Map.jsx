39c69472246fe1b23f0e17ff982c4b500556990f
KimDongSin <asstaff2020@gmail.com>
2024-06-18 23:57:23 +0900

    Feat: Home list

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -2,8 +2,9 @@ import React, { useEffect } from "react";
 
 const Map = () => {
   useEffect(() => {
+    const kakaoMapKey = import.meta.env.VITE_KAKAO_MAP_KEY;
     const script = document.createElement("script");
-    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=d4338524b2a6b4d4f30b172736982fc4&autoload=false`;
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoMapKey}&autoload=false`;
     document.head.appendChild(script);
 
     script.onload = () => {
@@ -18,25 +19,24 @@ const Map = () => {
         const map = new window.kakao.maps.Map(container, options);
 
         // 확대 축소 제어
-        const zoomControl = new window.kakao.maps.ZoomControl({
-          animation: window.kakao.maps.Animation.DROP, // 애니메이션 옵션 설정
-        });
+        const zoomControl = new window.kakao.maps.ZoomControl();
         map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
 
         // 마커 추가
-        const markerPosition = new window.kakao.maps.LatLng(
-          33.450701,
-          126.570667
-        );
+        const markerPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
         const marker = new window.kakao.maps.Marker({
           position: markerPosition,
         });
         marker.setMap(map);
       });
     };
+
+    return () => {
+      document.head.removeChild(script);
+    };
   }, []);
 
-  return <div id="map" style={{ width: "50%", height: "90vh" }}></div>;
+  return <div id="map" style={{ width: "100%", height: "500px" }}></div>;
 };
 
 export default Map;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,139 @@
+import styled from "styled-components";
 import Map from "../components/map/Map";
+
+const Wrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  .map__ul__wrap {
+    width: 400px;
+    height: 100vh;
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: auto;
+    padding: 20px 0;
+
+    &::-webkit-scrollbar {
+      width: 10px;
+    }
+    &::-webkit-scrollbar-track {
+      background-color: gray;
+    }
+    &::-webkit-scrollbar-thumb {
+      background-color: skyblue;
+    }
+    &::-webkit-scrollbar-button {
+      display: none;
+    }
+
+    &::-webkit-scrollbar-thumb:hover {
+      background-color: blue;
+      transition: all 0.2s; 
+    }
+
+    &::-webkit-scrollbar-track,
+    &::-webkit-scrollbar-thumb {
+      border-radius: 5px;
+    }
+
+    & > li:not(:last-child) {
+      margin-bottom: 20px;
+    }
+
+    li {
+      width: 343px;
+      height: 499px;
+      border: 1px solid #b2b9c0;
+      border-radius: 16px;
+      padding: 24px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+
+      & .date {
+        color: #b2b9c0;
+        font-size: 16px;
+        font-weight: 600;
+        line-height: 24px;
+        margin-top: 10px;
+      }
+
+      & .title {
+        color: #495057;
+        font-size: 20px;
+        font-weight: 600;
+        line-height: 32px;
+      }
+
+      & .desc {
+        color: #1b1d1f;
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 24px;
+      }
+
+      & .btn--add {
+        width: 295px;
+        height: 40px;
+        border-radius: 8px;
+        background-color: #495057;
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: none;
+        cursor: pointer;
+        font-size: 16px;
+        font-weight: 400;
+        margin-top: 24px;
+      }
+    }
+  }
+`;
 
 function Home() {
   return (
-    <>
+    <Wrap>
+      <ul className="map__ul__wrap">
+        <li>
+          <img src="http://via.placeholder.com/295x243" />
+          <div className="card__item">
+            <p className="date">6/17 ~ 6/19</p>
+            <p className="title">Product name</p>
+            <p className="desc">
+              This is a product description. It is best to keep it short,
+              between 1 or 3 lines.
+            </p>
+
+            <button className="btn--add" type="button">
+              Add to cart
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="http://via.placeholder.com/295x243" />
+          <div className="card__item">
+            <p className="date">6/17 ~ 6/19</p>
+            <p className="title">Product name</p>
+            <p className="desc">
+              This is a product description. It is best to keep it short,
+              between 1 or 3 lines.
+            </p>
+
+            <button className="btn--add" type="button">
+              Add to cart
+            </button>
+          </div>
+        </li>
+      </ul>
+
       <Map />
-    </>
-  )
+    </Wrap>
+  );
 }
 
 export default Home;
