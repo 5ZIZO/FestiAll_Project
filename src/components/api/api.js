1c67a64d4da93fc618eabdccfe52d30db1c1443e
Ghastly Mouse <gusdnr0839@gmail.com>
2024-06-19 19:19:49 +0900

    feat : 로그인한 유저 정보를 가져오는 함수 추가

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -19,3 +19,14 @@ export const getAnEvent = async ({ queryKey }) => {
     console.error(error);
   }
 };
+
+export const getCurrentUser = async () => {
+  try {
+    const { data: currentUser } = await supabase.auth.getUser();
+    console.log("현재 로그인한 유저 => ", currentUser);
+    return currentUser;
+  } catch (error) {
+    alert(error);
+    console.error("로그인한 유저 불러오기 실패 => ", error);
+  }
+};
