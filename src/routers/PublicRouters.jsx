102b8f3e6b10c873e31c87bc4900cb9a3ca98013
Ghastly Mouse <gusdnr0839@gmail.com>
2024-06-20 21:14:51 +0900

    add : 퍼블릭 라우터 추가

---
+++ src/routers/PublicRouters.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import useAuthStore from '../store/store';
+import { Navigate, Outlet } from 'react-router-dom';
+
+const PublicRouters = () => {
+    const isSignedIn = useAuthStore(state => state.isSignedIn);
+    console.log("isSignedIn => ", isSignedIn);
+    return (
+        <>
+            {isSignedIn ? <Navigate to="/" /> : <Outlet />}
+        </>
+    )
+}
+
+export default PublicRouters
