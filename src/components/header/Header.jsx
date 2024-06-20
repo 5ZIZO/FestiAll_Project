4b8205569903760cc074fd156a23ceaad85ce7bd
Captain-Kim <brighthero@kakao.com>
2024-06-20 16:27:45 +0900

    fix: pull 이후 헤더 버튼 navigate 경로 오류 수정

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -62,7 +62,7 @@ const Header = () => {
         ) : isSignedIn === false ? (
           <>
             <S.Nav to="/login">로그인</S.Nav>
-            <S.Nav to="/signup">회원가입</S.Nav>
+            <S.Nav to="/introduction">회원가입</S.Nav>
           </>
         ) : (
           <>
